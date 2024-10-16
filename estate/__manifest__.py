d1425c2b193e54e1559b844ef3daf6ff25cba5ce
bazene <bazenesergeamos0@gmail.com>
2024-10-16 11:50:37 +0200

    Integration of field

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,8 +1,10 @@
 {
     "name": "Real Estate",
     "summary": "Test Module",
-    "version": "18.0.1.0",
+    "version": "17.0.1.0",
     "licence": "OEEL-1",
     "depends": ["crm"],
+
+    "application": True,
     "data": [],
 }
